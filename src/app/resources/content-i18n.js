78da1311d7f02a2a79e55f8e3fec37ae2c285c1e
Ionas NEONAKIS <ionas.neonakis@gmail.com>
2025-04-29 21:04:07 +0200

    french version

--- src/app/resources/content-i18n.js
+++ src/app/resources/content-i18n.js
@@ -1,4 +1,4 @@
-import {addBasePath} from "@/app/utils/imageUtils";
+import { addBasePath } from "@/app/utils/imageUtils";
 
 const createI18nContent = (t) => {
     const person = {
@@ -7,15 +7,13 @@ const createI18nContent = (t) => {
         get name() {
             return `${this.firstName} ${this.lastName}`;
         },
-        role:  t("person.role"),
+        role: t("person.role"),
         avatar: addBasePath("/images/avatar.jpg"),
-        location: "Europe/Paris", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
-        languages: ["French", "English", "Greek"], // optional: Leave the array empty if you don't want to display languages
+        location: "Europe/Paris",
+        languages: ["Français", "Anglais", "Grec"],
     };
 
     const social = [
-        // Links are automatically displayed.
-        // Import new icons in /once-ui/icons.ts
         {
             name: "LinkedIn",
             icon: "linkedin",
@@ -35,139 +33,127 @@ const createI18nContent = (t) => {
 
     const home = {
         label: t("home.label"),
-        title: t("home.title", {name: person.name}),
-        description: t("home.description", {role: person.role}),
+        title: t("home.title", { name: person.name }),
+        description: t("home.description", { role: person.role }),
         headline: <>{t("home.headline")}</>,
-        subline: <>{t("home.subline")}</>
-    }
+        subline: <>{t("home.subline")}</>,
+    };
 
     const about = {
         label: t("about.label"),
         title: t("about.title"),
-        description: t("about.description", {name: person.name, role: person.role, location: person.location}),
-        tableOfContent: {
-            display: true,
-            subItems: true
-        },
-        avatar: {
-            display: true
-        },
-        calendar: {
-            display: true,
-            link: 'https://cal.com'
-        },
+        description: t("about.description", { name: person.name, role: person.role, location: person.location }),
+        tableOfContent: { display: true, subItems: true },
+        avatar: { display: true },
+        calendar: { display: false },
         intro: {
             display: true,
             title: t("about.intro.title"),
-            description: <>{t("about.intro.description")}</>
+            description: <>{t("about.intro.description")}</>,
         },
         work: {
-            display: true, // set to false to hide this section
+            display: true,
             title: t("about.work.title"),
             experiences: [
                 {
-                    company: 'FLY',
-                    timeframe: t("about.work.experiences.FLY.timeframe"),
-                    role: t("about.work.experiences.FLY.role"),
-                    achievements: t("about.work.experiences.FLY.achievements").split(";"),
-                    images: [ // optional: leave the array empty if you don't want to display images
+                    company: "Leader du Luxe",
+                    timeframe: t("about.work.experiences.Leader du Luxe.timeframe"),
+                    role: t("about.work.experiences.Leader du Luxe.role"),
+                    achievements: t("about.work.experiences.Leader du Luxe.achievements").split(";"),
+                    images: [
                         {
-                            src: addBasePath('/images/projects/project-01/cover-01.jpg'),
-                            alt: 'Once UI Project',
+                            src: addBasePath("/images/projects/project-01/cover-01.jpg"),
+                            alt: "Projet Leader du Luxe",
                             width: 16,
-                            height: 9
-                        }
-                    ]
+                            height: 9,
+                        },
+                    ],
                 },
                 {
-                    company: 'Creativ3',
-                    timeframe: t("about.work.experiences.Creativ3.timeframe"),
-                    role: t("about.work.experiences.Creativ3.role"),
-                    achievements: t("about.work.experiences.Creativ3.achievements").split(";"),
-                    images: [ ]
+                    company: "Takima",
+                    timeframe: t("about.work.experiences.Takima.timeframe"),
+                    role: t("about.work.experiences.Takima.role"),
+                    achievements: t("about.work.experiences.Takima.achievements").split(";"),
+                    images: [],
+                },
+                {
+                    company: "Atos",
+                    timeframe: t("about.work.experiences.Atos.timeframe"),
+                    role: t("about.work.experiences.Atos.role"),
+                    achievements: t("about.work.experiences.Atos.achievements").split(";"),
+                    images: [],
                 }
-            ]
+            ],
         },
         studies: {
-            display: true, // set to false to hide this section
+            display: true,
             title: t("about.studies.title"),
             institutions: [
                 {
-                    name: t('about.studies.institutions.MastersUniversityOrleans.name'),
-                    description: <>{t(`about.studies.institutions.MastersUniversityOrleans.description`)}</>,
+                    name: "Université d'Orléans",
+                    description: <>{t("about.studies.institutions.Université d'Orléans.description")}</>,
                 },
                 {
-                    name:  t('about.studies.institutions.LicenseUniversityOrleans.name'),
-                    description: <>{t(`about.studies.institutions.LicenseUniversityOrleans.description`)}</>,
+                    name: "Université d'Orléans (Licence)",
+                    description: <>{t("about.studies.institutions.Université d'Orléans (Licence).description")}</>,
                 },
-            ]
+            ],
         },
         technical: {
-            display: true, // set to false to hide this section
+            display: true,
             title: t("about.technical.title"),
             skills: [
                 {
-                    title: 'React',
+                    title: "SpringBoot",
+                    description: <>{t("about.technical.skills.SpringBoot.description")}</>,
+                    images: [
+                        {
+                            src: addBasePath("/images/projects/project-01/cover-02.jpg"),
+                            alt: "Spring Boot",
+                            width: 16,
+                            height: 9,
+                        },
+                    ],
+                },
+                {
+                    title: "React",
                     description: <>{t("about.technical.skills.React.description")}</>,
                     images: [
                         {
-                            src: addBasePath('/images/projects/project-01/cover-02.jpg'),
-                            alt: 'Project image',
+                            src: addBasePath("/images/projects/project-01/cover-03.jpg"),
+                            alt: "React",
                             width: 16,
-                            height: 9
+                            height: 9,
                         },
-                        {
-                            src: addBasePath('/images/projects/project-01/cover-03.jpg'),
-                            alt: 'Project image',
-                            width: 16,
-                            height: 9
-                        },
-                    ]
+                    ],
                 },
                 {
-                    title: 'Spring Boot',
-                    description: <>{t("about.technical.skills.Spring Boot.description")}</>, // "." not accepted in next-intl namespace
-                    images: [
-                        {
-                            src: addBasePath('/images/projects/project-01/cover-04.jpg'),
-                            alt: 'Project image',
-                            width: 16,
-                            height: 9
-                        },
-                    ]
-                },
-                {
-                    title: 'DevOps',
+                    title: "DevOps",
                     description: <>{t("about.technical.skills.DevOps.description")}</>,
                     images: [
                         {
-                            src: addBasePath('/images/projects/project-01/cover-04.jpg'),
-                            alt: 'Project image',
+                            src: addBasePath("/images/projects/project-01/cover-04.jpg"),
+                            alt: "DevOps",
                             width: 16,
-                            height: 9
+                            height: 9,
                         },
-                    ]
+                    ],
                 }
-
-            ]
-        }
-    }
+            ],
+        },
+    };
 
     const blog = {
         label: t("blog.label"),
         title: t("blog.title"),
-        description: t("blog.description", {name: person.name})
-        // Create new blog posts by adding a new .mdx file to app/blog/posts
-        // All posts will be listed on the /blog route
-    }
+        description: t("blog.description", { name: person.name }),
+    };
 
     const work = {
         label: t("work.label"),
         title: t("work.title"),
-        description: t("work.description", {name: person.name})
-        // Create new project pages by adding a new .mdx file to app/blog/posts
-        // All projects will be listed on the /home and /work routes
-    }
+        description: t("work.description", { name: person.name }),
+    };
 
     return {
         person,
@@ -176,7 +162,7 @@ const createI18nContent = (t) => {
         about,
         blog,
         work,
-    }
+    };
 };
 
 export { createI18nContent };
